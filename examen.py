bc23b320a03cb476d29b211b317a279fc692efb6
Diana Ivonee Huitzil Sosa <31254618+divoneehs@users.noreply.github.com>
2017-09-12 13:35:02 -0500

    Add files via upload
    Modifico el archivo porque el primero no era usando funciones

--- examen.py
+++ examen.py
@@ -1,10 +1,11 @@
-import pylab as pl
+import numpy as np
 import matplotlib.pyplot as plt
-x = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-y = [1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017 ]
-pl.plot(x, y, 'xg')
+def y(x):
+	return (x) + 1997
+x= np.arange(0.0,20.0,1.0)
+plt.plot(x, y(x),'*g')
 plt.title("Diana Ivonee Huitzil Sosa")
 plt.xlabel("Edad")
 plt.ylabel("Anio")
 plt.savefig('edadIvonee.png')
-plt.show
+plt.show ()
