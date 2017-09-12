6ca1a0bb0fc3600fc943e32a3e121c89253aa9b6
Diana Ivonee Huitzil Sosa <31254618+divoneehs@users.noreply.github.com>
2017-09-12 13:10:43 -0500

    Add files via upload

--- examen.py
+++ examen.py
@@ -2,7 +2,7 @@ import pylab as pl
 import matplotlib.pyplot as plt
 x = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 y = [1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017 ]
-pl.plot(x, y, '*r')
+pl.plot(x, y, 'xg')
 plt.title("Diana Ivonee Huitzil Sosa")
 plt.xlabel("Edad")
 plt.ylabel("Anio")
